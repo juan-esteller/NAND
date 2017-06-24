48ce54c6a4efbd13cf35f2c24fc9963d9d5b9b3d
Juan Esteller <esteller@college.harvard.edu>
2017-06-23 22:28:51 -0400

    added more syntactic sugar

--- SS.ml
+++ SS.ml
@@ -1,7 +1,14 @@
 open PL_functor 
 
+(* DECLARATIONS OF HELPER FUNCTIONS  *) 
+
+(* parses a program as a string *) 
 let parseStr (s: string): program  = 
   NANDparser.parseProg NANDlexer.token (Lexing.from_string s) 
+
+(* utility function to generate a line of NAND code *) 
+let genLine (id: varID) (l: string) (r: string)  : program = 
+  parseStr ((strOfId id)^" := "^l^" NAND "^r^"\n")
 
 (* utility function to generate a unique fresh variable, used throughout *) 
 let freshVar : unit -> varID = 
@@ -9,33 +16,47 @@ let freshVar : unit -> varID =
     fun () -> 
      let ind, _ = Int(!cell), (cell := !cell + 1) in
        ("unique", ind)  
-(* utility function to generate a line of NAND code *) 
-let genLine (id: varID) (l: string) (r: string)  : program = 
-  parseStr ((strOfId id)^" := "^l^" NAND "^r^"\n")
- 
-let isUnary (e: exp) : bool = 
+
+(* determines whether an expression is simply a value *)  
+let isValue (e: exp) : bool = 
   match e with 
   | Var(_x) -> true 
   | Const(_x) -> true 
   | IsValid(_x) -> true
-  | _ -> false  
-(*
-  Function to enable direct assignment (i.e. a := b)  
-  ASSUMPTIONS: 
-    - No function applications 
-*) 
-let enableAsg (c: command) : program = 
+  | _ -> false
+
+(* maps a macro that maps a command to a sequence of commands to entire program *) 
+let mapToProg (macro: command -> program) (p: program): program = 
+  List.concat (List.map macro p)  
+
+(* END OF HELPER FUNCTIONS *) 
+
+(* START OF MACROS, in order of increasing complexity *) 
+
+(* macro to expand list of assignments to lines *) 
+let unzipCom (c: command) : program = 
+  let pair (v: varID) (e: exp) : command = Asg([v], [e]) in 
+    match c with 
+    | Asg(vars, exps) -> 
+       List.map2 pair vars exps 
+    | _ -> [c]
+
+let unzipProg (p: program) : program = 
+  mapToProg unzipCom p
+
+(* enables direct assignment (i.e. a := b)  *) 
+let enableAsgCom (c: command) : program = 
   match c with 
   | Asg([u], [e]) -> 
-     if not (isUnary e) then 
+     if not (isValue e) then 
       [c]
      else let newVar, eStr =  freshVar (), strOfExp e in 
        let newVarStr = strOfId newVar in 
          (genLine newVar eStr eStr) @ (genLine u newVarStr newVarStr) 
   | _ -> [c]
 
-let enableAsg (p: program) : program = 
-  List.concat (List.map enableAsg p) 
+let enableAsgProg (p: program) : program = 
+  mapToProg enableAsgCom p
 (* 
 let constProg = 
   "notx_0 := x_0 NAND x_0
