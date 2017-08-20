59c6e2dbd56d72b4ef34a61fd82af7c51b23318b
Juan Esteller <esteller@college.harvard.edu>
2017-08-20 16:41:53 -0400

    updated shell script for build to check for dependencies

---
+++ nand-pl/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# function to check that all dependencies are installed
+function check_installs { 
+  DEPENDS=$@
+ 
+  echo "Checking that dependencies of NAND are installed"  
+  for DEPEND in $DEPENDS 
+  do 
+    if ! ls /usr/bin | grep -q $DEPEND; then
+      echo "It looks like you don't have "$DEPEND" installed-- aborting build." ;
+      exit 1 ;
+    fi 
+  done 
+  echo "All dependencies are installed" 
+}
+
+check_installs ocaml ocamlbuild ocamlyacc ocamllex
+
+# actual build 
+
+# location of source code 
+INCLUDE_DIRS=src,parsing 
+
+echo "Starting build" 
+if ocamlbuild main.native -Is $INCLUDE_DIRS ; then 
+  echo "Build succeeded" 
+else 
+  echo "Build failed" 
+fi 
